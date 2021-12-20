7031bd81a6b4b1cbf616e8cf43c09080dbcce2fa
Paweł K <grylion02@gmail.com>
2021-12-20 17:30:02 +0100

    add test data and url class

---
+++ 4_SeleniumAndSelenideProject/src/test/java/data/TestUrls.java
@@ -0,0 +1,8 @@
+package data;
+
+public class TestUrls {
+
+    public static final String BASE_URL = "http://automationpractice.com/";
+    public static final String LOGIN_PAGE_URL = BASE_URL + "index.php?controller=authentication&back=my-account";
+
+}
