de2ec8174f4895b9ef40da9aa4d7fe8bc4bb9d66
Paweł K <grylion02@gmail.com>
2021-12-27 17:02:39 +0100

    changes

--- 4_SeleniumAndSelenideProject/src/test/java/data/TestData.java
+++ 4_SeleniumAndSelenideProject/src/test/java/data/TestData.java
@@ -4,5 +4,6 @@ public class TestData {
 
     public static String userName = "fbn33265@bcaoo.com";
     public static String userPassword = "Test1234";
+    public static String userDisplayedName = "Test test";
 
 }
